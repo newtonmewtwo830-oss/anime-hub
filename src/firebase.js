7d4d308d5e78e07f96a96fb692ff78f00ef2d0a0
Rupesh Kumar <newtonmewtwo830@gmail.com>
2025-12-10 20:34:23 +0530

    Fix firebase config to use Vercel env vars

--- src/firebase.js
+++ src/firebase.js
@@ -1,16 +1,16 @@
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyBtjkb2CrAlktD5txiuzUW_PrKpL3RhZtI",
-  authDomain: "anime-hub-ae9f6.firebaseapp.com",
-  databaseURL: "https://anime-hub-ae9f6-default-rtdb.firebaseio.com",
-  projectId: "anime-hub-ae9f6",
-  storageBucket: "anime-hub-ae9f6.firebasestorage.app",
-  messagingSenderId: "842177234766",
-  appId: "1:842177234766:web:e21a75eaa1ee190b68401f",
-  measurementId: "G-62KL87FC43"
+  apiKey: import.meta.env.VITE_API_KEY,
+  authDomain: import.meta.env.VITE_AUTH_DOMAIN,
+  databaseURL: import.meta.env.VITE_DATABASE_URL,
+  projectId: import.meta.env.VITE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_APP_ID,
+  measurementId: import.meta.env.VITE_MEASUREMENT_ID
 };
+
 const app = initializeApp(firebaseConfig);
 export const db = getFirestore(app);
